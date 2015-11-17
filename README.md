8f7475483c9696ab15ee882cdbb6788ec4e9feb5
Corey Watts <cwatts@brightidea.com>
2015-11-16 21:20:37 -0800

    Puddin is live!

---
+++ README.md
@@ -0,0 +1,9 @@
+# Puddin
+
+Everyone loves pudding.
+
+## Instructions
+Clone this repository, cd into the directory, and execute the pudding.sh script.
+
+### NOTE:
+This only works on OS X.
---
+++ pudding.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# A poem posted on
+# https://www.reddit.com/r/ShittyPoetry/comments/3t2qje/how_do_you_describe_pudding/
+# Written by frankieh: https://www.reddit.com/user/frankieh
+
+say <<PUDDIN
+Slimy gooey
+it’s like soft jell-o
+like chocolate jell-o,
+I can’t describe it,
+it’s like a color
+Blue is blue
+And pudding is pudding
+Can you describe it?
+It’s like I can’t use words
+To describe what it is
+A person can’t know
+What pudding really is
+Without a visual, it’s impossible to describe
+The consistency, the taste, only exist in our minds
+In our minds, pudding’s a thing,
+But I know you can’t describe it
+To someone who doesn’t know
+Pudding is just jell-o
+I can only come up
+with similar things
+I can tell you what it’s not,
+it’s not chicken wings
+It’s not tapioca, it’s not a fruit
+it’s not liquid and it’s not a boot
+It’s like it’s knowledge that exists,
+only in reality, but not in abstract
+In our minds, we’re limited
+PUDDIN
